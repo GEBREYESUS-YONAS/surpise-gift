fb7ee19fd63bf40168555c63c33fd5408f2f1bd2
GEBREYESUS-YONAS <148392550+GEBREYESUS-YONAS@users.noreply.github.com>
2024-09-13 16:20:31 +0100

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function addToCart(item) {
+    // For demonstration purposes: log to console
+    console.log(`${item} added to cart!`);
+    // Implement cart functionality here
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript is working!');
+});
